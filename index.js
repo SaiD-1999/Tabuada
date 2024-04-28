6157b9a7c2af7b129bccafba8517ea4696494401
Hugo Dias <hugo-souza1999@outlook.com>
2024-04-28 19:10:21 -0300

    Em desenvolvimento

--- index.js
+++ index.js
@@ -8,15 +8,31 @@ function paginaInicial(requisicao, resposta){
     resposta.write('<html>');
     resposta.write('<head>');
     resposta.write('<meta charset="utf-8">');
-    resposta.write('<title>Tabuada</title>');
+    resposta.write('<title>Página Inicia</title>');
     resposta.write('</head>');
     resposta.write('<body>');
-    resposta.write('<h1>Página Inicial</h1>');
+    resposta.write('<h1>Tabuada</h1>');
     resposta.write('</body>');
     resposta.write('</html>');
     resposta.end();
 };
 
+function tabuadas(reqisicao, resposta){
+    let valor = reqisicao.query.numero;
+    resposta.write('<!DOCTYPE html>');
+    resposta.write('<html>');
+    resposta.write('<head>');
+    resposta.write('<meta charset="utf-8">');
+    resposta.write('<title>Página Inicia</title>');
+    resposta.write('</head>');
+    resposta.write('<body>');
+    resposta.write('<h1>Tabuada</h1>');
+    resposta.write('</body>');
+    resposta.write('</html>');
+    resposta.end();
+};
+
+app.get("/tabuada", tabuadas);
 app.get("/", paginaInicial);
 app.listen(porta, host, () => {
     console.log("Servidor esta executando em http://" + host + ":" + porta);
